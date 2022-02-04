0e1f2ff01a73a698bdedae082b5ed4c85477d5b4
DESKTOP-PC\Super User <abcinc@localhost>
2022-02-04 06:37:57 -0500

    done

--- libraries/splunk/steps/splunk_step_watcher.groovy
+++ libraries/splunk/steps/splunk_step_watcher.groovy
@@ -4,7 +4,8 @@ void before(){
 }
 
 //@AfterStep
-@AfterStep({ hookContext.step.equals("static_code_analysis") })
+//@AfterStep({ hookContext.step.equals("static_code_analysis") })
+@AfterStep({ hookContext.step in config.afterSteps })
 void after(){
     println "Splunk: running after the ${hookContext.library} library's ${hookContext.step} step"
 }
